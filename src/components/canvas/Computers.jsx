d74952e484b61a3b2a256c1742b87feb0080ce8c
AimTheSun <mirassol.pedro7@gmail.com>
2025-02-23 12:54:01 +0000

    feat: computer model done for mobile and desktop

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,12 +8,12 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
+      <hemisphereLight intensity={1} groundColor="black" />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
-        intensity={1}
+        intensity={5}
         castShadow
         shadow-mapSize={1024}
       />
@@ -29,9 +29,33 @@ const Computers = ({ isMobile }) => {
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    
+    // set the initial value of isMobile state variable
+    setIsMobile(mediaQuery.matches);
+    
+    
+    // Define a callback function to handle the changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
-      frameloop="demand"
+      frameLoop="demand"
       shadows
       camera={{ position: [20, 3, 5], fov: 25 }} // camera position and field of view
       gl={{ preserveDrawingBuffer: true }} // preserve canvas after unmount
@@ -42,7 +66,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
