9f45dbbbd9b80407d6f21c89cb5fd8347cf61e4f
AimTheSun <mirassol.pedro7@gmail.com>
2025-02-24 22:10:33 +0000

    feat: added Education sector

--- src/assets/index.js
+++ src/assets/index.js
@@ -25,6 +25,8 @@ import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
+import codeforall from "./company/codeforall.png";
+import coimbra from "./company/coimbra.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -59,4 +61,6 @@ export {
   carrent,
   jobit,
   tripguide,
+  codeforall,
+  coimbra
 };
